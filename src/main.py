37636cdab633b6cbf383f28e76a2145ae1c7d586
sgonzalo <santiago.gonzalo360@gmail.com>
2020-01-17 02:04:01 +0000

    2nd commit

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from models import db
-#from models import Person
+from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -27,6 +27,51 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+@app.route('/person', methods=['GET','POST'])
+def handle_query():
+    if request.method == 'GET':
+        people_query = Person.query.all()
+        all_people = list(map(lambda x: x.serialize(), people_query))
+        return jsonify(all_people), 200
+    if request.method =='POST':
+
+        body = request.get_json()
+        user1 = Person(username=body.get('username'), email=body.get('email'),address=body.get('address'),full_name=body.get('full_name'),phone=body.get('phone'))
+        db.session.add(user1)
+        db.session.commit()
+        return jsonify(user1.id), 200
+
+@app.route('/person/<int:id>', methods=['GET','PUT','DELETE'])
+def handle_person(person_id):
+    if request.method == 'GET':
+        user1 = Person.query.get(person_id)
+        return jsonify(all_people), 200
+
+    if request.method == 'PUT':
+        
+        if user1 is None:
+            raise APIException('User not found', status_code=404)
+
+    if "username" in body:
+        user1.username = body["username"]
+    if "email" in body:
+        user1.email = body["email"]
+    if "phone" in body:
+        user1.email = body["phone"]
+    if "full_name" in body:
+        user1.email = body["full_name"]
+    if "address" in body:
+        user1.email = body["address"]
+    db.session.commit()
+    
+    if request.method =='DELETE':
+        user1 = Person.query.get(person_id)
+    if user1 is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(user1)
+    db.session.commit()
+    return jsonify(user1), 200
 
 @app.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
--- src/models.py
+++ src/models.py
@@ -2,16 +2,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(80), unique=True, nullable=False)
+    address = db.Column(db.String(120), unique=True, nullable=False)
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return '<Person %r>' % self.username
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "full_name": self.full_name,
+            "username": self.username,
+            "email": self.email,
+            "phone": self.phone,
+            "address": self.address,
+        }
